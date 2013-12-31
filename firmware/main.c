8f6ccdaccf624a3e2c727ff4e6ad2c7dc479b51b
cpldcpu <cpldcpu@gmail.com>
2013-12-31 13:34:12 +0100

    firmware: leavebootloader does never return
    The compiler does only understand this when inserting an infinite loop.
    Saves two bytes and prevents a warning.

--- firmware/main.c
+++ firmware/main.c
@@ -261,10 +261,11 @@ static inline void leaveBootloader(void) {
       nop();
     }
   #endif
-
-  // jump to application reset vector at end of flash
-  asm volatile ("rjmp __vectors - 4");
-  }
+  
+  asm volatile ("rjmp __vectors - 4"); // jump to application reset vector at end of flash
+  
+  for (;;); // Make sure function does not return to help compiler optimize
+}
 
 int main(void) {
     
