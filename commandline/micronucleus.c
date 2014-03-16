a6ddfb278eba3c2864530458e46716ebb6b18c7b
cpldcpu <cpldcpu@gmail.com>
2014-03-16 09:34:29 +0100

    commandline: Display correct help for windows

--- commandline/micronucleus.c
+++ commandline/micronucleus.c
@@ -73,9 +73,9 @@ int main(int argc, char **argv) {
   int file_type = FILE_TYPE_INTEL_HEX;
   int arg_pointer = 1;
   #if defined(WIN)
-  char* usage = "usage: micronucleus [--help] [--run] [--dump-progress] [--fast-mode] [--type intel-hex|raw] [--no-ansi] [--timeout integer] [--erase-only] filename";
-  #else
   char* usage = "usage: micronucleus [--help] [--run] [--dump-progress] [--fast-mode] [--type intel-hex|raw] [--timeout integer] [--erase-only] filename";
+  #else
+  char* usage = "usage: micronucleus [--help] [--run] [--dump-progress] [--fast-mode] [--type intel-hex|raw] [--timeout integer] [--erase-only] filename [--no-ansi]";
   #endif 
   progress_step = 0;
   progress_total_steps = 5; // steps: waiting, connecting, parsing, erasing, writing, (running)?
