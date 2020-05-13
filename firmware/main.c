def9556fcf1c11bb8f3f416a786e6b0a6571b0ad
Armin <armin.joachimsmeyer@gmail.com>
2020-05-13 11:34:53 +0200

    Initial 2.5 version

--- firmware/main.c
+++ firmware/main.c
@@ -11,7 +11,7 @@
  *
  *  This file is part of micronucleus https://github.com/micronucleus/micronucleus.
  *
- *  ServoEasing is free software: you can redistribute it and/or modify
+ *  Micronucleus is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 2 of the License, or
  *  (at your option) any later version.
