a8a65708631401d8582e9b551a5a2fff5ba412cb
Armin <armin.joachimsmeyer@gmail.com>
2021-01-31 11:18:49 +0100

    corrected typo and removed ZIPFILE for static MAC OS Binary

--- commandline/micronucleus.c
+++ commandline/micronucleus.c
@@ -4,7 +4,7 @@ Created: September 2012
   Changes for Micronucleus protocol version V2.x
   (c) 2014 T. Bo"scke
   Improved error messages version 2.5
-  (c) 2021 T. ArminJo
+  (c) 2021 ArminJo
 
   Permission is hereby granted, free of charge, to any person obtaining a copy of
   this software and associated documentation files (the "Software"), to deal in
