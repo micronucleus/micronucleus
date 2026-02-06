2bb2d45f3b5414eed8e693a6f32d9ac48300c29e
Simon Biewald <simon@fam-biewald.de>
2026-02-07 00:48:50 +0100

    Remove long device description, it would cost 23 bytes.

--- firmware/usbconfig.h
+++ firmware/usbconfig.h
@@ -99,8 +99,8 @@
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME 'M', 'i', 'c', 'r', 'o', 'n', 'u', 'c', 'l', 'e', 'u', 's', ' ', 'B', 'o', 'o', 't', 'l', 'o', 'a', 'd', 'e', 'r'
-#define USB_CFG_DEVICE_NAME_LEN 23
+//#define USB_CFG_DEVICE_NAME 'M', 'i', 'c', 'r', 'o', 'n', 'u', 'c', 'l', 'e', 'u', 's', ' ', 'B', 'o', 'o', 't', 'l', 'o', 'a', 'd', 'e', 'r'
+//#define USB_CFG_DEVICE_NAME_LEN 23
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
